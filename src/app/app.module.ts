355940d3cc511cee83c2570c69ad32f76b0a9035
mrdietz <mrdietz@gmail.com>
2021-04-26 12:06:06 -0700

    start basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginBoxComponent } from './components/login-box/login-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginBoxComponent
   ],
   imports: [
     BrowserModule,
